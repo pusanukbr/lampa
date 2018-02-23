e8b9bbe06af6e0110ea94ddad7313271b8344af7
Bohdan Pusanuk <pusanukbr@live.ru>
2018-02-23 19:42:30 +0200

    logo and video

--- js/common.js
+++ js/common.js
@@ -23,7 +23,7 @@ $('.btn_mobile_exit').click(function() {
 
 // Плавный скролл
 $(function() {
-  $('.dots_navigation ul li a').click(function() {
+  $('body a').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
